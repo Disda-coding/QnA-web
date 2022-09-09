43006bb02c09e8ef7917a37e51e64033ca25871f
Disda-coding <497457669@qq.com>
2022-09-09 11:25:44 +0800

    Initial commit

---
+++ src/main/java/com/disda/qnaweb/QnAWebApplication.java
@@ -0,0 +1,13 @@
+package com.disda.qnaweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QnAWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QnAWebApplication.class, args);
+    }
+
+}
